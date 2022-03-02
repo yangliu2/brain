84cb84ae79e75826c8e8aff96152b49bd4faadf4
Yang Liu <yangliu3456@gmail.com>
2022-03-01 21:56:54 -0500

    use mwparserfrom hell

--- brain/wiki/wiki_reader.py
+++ brain/wiki/wiki_reader.py
@@ -1,12 +1,28 @@
 from brain import Database
+import mwparserfromhell
 
 
-def get_text():
-    query = '''
-            SELECT CONVERT(old_text USING utf8) 
-            FROM text 
-            WHERE old_id=1071857811;
-            '''
-    database = Database()
-    result = database.query(query=query)
-    return result
+class WikiParser:
+
+    def __init__(self,
+                 database: str,
+                 query: str) -> None:
+        self.database = database
+
+
+    def get_text(self, 
+                 query: str):
+        query = '''
+                SELECT CONVERT(old_text USING utf8) 
+                FROM text 
+                WHERE old_id=1071857811;
+                '''
+        database = Database(database=self.database)
+        result = database.query(query=query)
+        return result
+
+    def convert_wiki_to_text(self):
+        wiki_text = self.get_text()
+        parsed_wikicode = mwparserfromhell.parse(wiki_text)
+        read_text = parsed_wikicode.strip_code()
+        return read_text
